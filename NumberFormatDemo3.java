2f610d995e8e5c6ba1f879ba411b066eceed167f
PrabudeepSingh <prabhudeepsb@gmail.com>
2017-11-21 23:00:54 +0530

    To illustrate the concept of NumberFormat in java.

---
+++ NumberFormatDemo3.java
@@ -0,0 +1,26 @@
+import java.text.*;
+import java.util.*;
+
+class NumberFormatDemo3
+{
+	public static void main(String[] args) 
+	{
+		NumberFormat nf  = NumberFormat.getInstance();
+
+		nf.setMaximumFractionDigits(2);
+		System.out.println(nf.format(123.4567));
+		System.out.println(nf.format(123.4));
+
+		nf.setMinimumFractionDigits(2);
+		System.out.println(nf.format(123.4567));
+		System.out.println(nf.format(123.4));
+
+		nf.setMaximumIntegerDigits(3);
+		System.out.println(nf.format(123456.789));
+		System.out.println(nf.format(1.2345));
+
+		nf.setMinimumIntegerDigits(3);
+		System.out.println(nf.format(123456.789));
+		System.out.println(nf.format(1.2345));
+	}
+}
